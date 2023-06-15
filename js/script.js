dee2abf67946c6358a9011d60b286b71c7a34f11
Alpesh1999 <89983218+Alpesh1999@users.noreply.github.com>
2023-06-15 19:22:09 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let loginBtn = document.querySelector("#loginBtn");
+let formPopup = document.querySelector("#formPopup");
+let closeForm = document.querySelector("#closeForm");
+
 function toggleBar() {
   let navItems = document.querySelector("#navItems");
   let toggleBarIcon = document.querySelector("#toggleBar");
@@ -5,9 +9,31 @@ function toggleBar() {
   toggleBarIcon.classList.toggle("toggleBarIcon");
 }
 
-// let navLink = document.querySelectorAll(".navLink");
-// let navItems = document.querySelector("#navItems");
+loginBtn.addEventListener("click", loginForm);
 
-// navLink.addEventListner("click", function () {
-//   navItems.classList.remove("mobile_navItems");
-// });
+function loginForm() {
+  formPopup.style.display = "block";
+}
+
+closeForm.addEventListener("click", closeform);
+
+function closeform() {
+  formPopup.style.display = "none";
+}
+
+function openLogin(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
